f4e52ca3ada3a52e4ecd6281d72a003285303192
jeycee <jeyceejeyka635@gmail.com>
2024-12-04 23:42:18 +0300

    Adds array find method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const games = [
+    { year: 2018, result: "L" },
+    { year: 2019, result: "W" },
+    { year: 2020, result: "L" },
+  ];
+  function superbowlWin(games) {
+    const win = games.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  console.log(superbowlWin(games)); // Output: 2019
+  
